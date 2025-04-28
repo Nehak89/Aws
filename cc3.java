c6e44709d8ddfbdff784eb84a45dcc2c3617043c
NehaKulkarni28 <158441391+Nehak89@users.noreply.github.com>
2025-04-28 15:03:01 +0530

    Update cc3.java

--- cc3.java
+++ cc3.java
@@ -1,18 +1,19 @@
 public class second{
          public static void sendMail(String address,String subject,String body){
 		 Messaging.SingleEmailMessage mail=new Messaging.SingleEmailMessage();
-		 String [] toAddresses=new String[]{addresses};
+		 String [] toAddresses=new String[]{address};
 		 mail.setToAddresses(toAddresses);
 		 mail.setSubject(subject);
 		 mail.setPlainTextBody(body);
-		 Messaging.SingleEmailMessage(new Messaging.SingleEmailMessage[]{mail});
-		 //Messaging.sendEmail(new Messaging.SingleEmailMessage[]{mail});
+		 //Messaging.SingleEmailMessage(new Messaging.SingleEmailMessage[]{mail});
+		 Messaging.sendEmail(new Messaging.SingleEmailMessage[]{mail});
 	}
+}
 	
 	
 	
 	//Apex code(write this code in execution cell)
-	String Addresses="recipients addresses";//give emailId
+	String address="recipients address";//give your emailId
 	String subject="LP-2 assignment confirmation";
 	String body="Cloud Computing Assignment 3 implemented successfully';
 	second.sendMail(address,subject,body);
